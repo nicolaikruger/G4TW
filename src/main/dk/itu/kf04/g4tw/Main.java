d2eb3a240c843ed41253eb2c77402dd80ae9d908
Jegp <jensep@gmail.com>
2012-03-21 13:04:01 +0100

    Added getter and setters to WebServer

--- src/main/dk/itu/kf04/g4tw/Main.java
+++ src/main/dk/itu/kf04/g4tw/Main.java
@@ -2,12 +2,20 @@ package dk.itu.kf04.g4tw;
 
 import dk.itu.kf04.g4tw.controller.WebServer;
 
+/**
+ * Main entry-point for the application.
+ */
 public class Main {
 
+    /**
+     * Starts the application by initializing data and then starting server.
+     * @param args  The arguments to feed the application.
+     */
     public static void main(String[] args) {
-        System.out.println(WebServer.init());
-    }
+        // Import data
 
-    public static int output() { return 1; }
+        // Init server
+        boolean success = WebServer.init();
+    }
 
 }
--- src/main/dk/itu/kf04/g4tw/controller/WebServer.java
+++ src/main/dk/itu/kf04/g4tw/controller/WebServer.java
@@ -24,33 +24,42 @@ public class WebServer implements HTTPConstants {
     private static int port = 8080;
     
     /**
-     * Initialize the server
+     * Initialize the server and prepare to respond on incoming requests.
+     *
+     * @return  boolean  A boolean flag indicating success or failure
      */
     public static boolean init() {
+        // Return if webserver already has been started
         if (isInitialized == true) return false;
 
+        // Try to initialize a ServerSocket
         try {
             ServerSocket server = new ServerSocket(port);
             isInitialized = true;
 
-            while (server.isBound()) {
+            // Loop
+            while (true) {
+                // Wait for a connection
                 Socket s = server.accept();
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String request = in.readLine();
+                System.out.println("Request: " + request);
+                s.shutdownInput();
+
                 String contenttype = URLConnection.guessContentTypeFromName("C:/workspace/G4TW/index.html");
                 File file = new File("www/index.html");
                 InputStream fileIn = new FileInputStream(file);
                 OutputStream out = new BufferedOutputStream(s.getOutputStream());
                 PrintStream pout = new PrintStream(out);
 
-                String request = in.readLine();
-                System.out.println("Request: " + request);
-                s.shutdownInput();
-
                 byte[] buffer = new byte[1000];
                 while(fileIn.available() > 0) {
                     out.write(buffer, 0, fileIn.read(buffer));
                 }
                 pout.flush();
+                
+                // Close down the socket
                 s.shutdownOutput();
                 s.close();
             }
@@ -63,6 +72,20 @@ public class WebServer implements HTTPConstants {
             e.printStackTrace();
         }
         return isInitialized;
+    }
+
+    /**
+     * Returns the port of the WebServer.
+     * @return int  The port the WebServer listens to when initialized.
+     */
+    public static int getPort() { return port; }
+    
+    /**
+     * Sets the port of the WebServer.
+     * @param port  The port between 0 and 0xFFFF
+     */
+    public static void setPort(int port) {
+        WebServer.port = port;
     }
 
 }
