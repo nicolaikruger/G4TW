9b20ba3f27943ab6e8b3f020e75c08983b1b54f1
Jegp <jensep@gmail.com>
2012-04-01 12:17:35 +0200

    Added a MapModel.

---
+++ src/main/dk/itu/kf04/g4tw/model/MapModel.java
@@ -0,0 +1,14 @@
+package dk.itu.kf04.g4tw.model;
+
+/**
+ * A map
+ */
+public class MapModel {
+    
+    public static final int HIGHWAY = 0;
+    public static final int ROAD    = 1;
+    // ... //
+
+
+
+}
