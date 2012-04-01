20a69fabaa557665dd35b42c4f85f3c67ec0c013
Jegp <jensep@gmail.com>
2012-04-01 12:46:43 +0200

    Renamed MapPart to RoadTypeTree Implemented quick maps for road id => road type and road type => RoadTypeTree

--- src/main/dk/itu/kf04/g4tw/model/RoadTypeTree.java
+++ src/main/dk/itu/kf04/g4tw/model/RoadTypeTree.java
@@ -5,7 +5,7 @@ import dk.itu.kf04.g4tw.model.tree.Tree2D;
 /**
  * A part of a larger map containing only the specified entity-types.
  */
-public class MapPart extends Tree2D {
+public class RoadTypeTree extends Tree2D {
 
     public final int type;
 
@@ -13,7 +13,7 @@ public class MapPart extends Tree2D {
      * Creates a new part of a map.
      * @param type  The entity-type this map-part contains.
      */
-    public MapPart(int type) {
+    public RoadTypeTree(int type) {
         this.type = type;
     }
 
