984e95992c12a6614ba2e290d91c3522c90e4fef
Jegp <jensep@gmail.com>
2012-04-02 11:40:33 +0200

    Added logging in the Main class.

--- src/main/dk/itu/kf04/g4tw/Main.java
+++ src/main/dk/itu/kf04/g4tw/Main.java
@@ -5,28 +5,43 @@ import dk.itu.kf04.g4tw.model.MapModel;
 import dk.itu.kf04.g4tw.model.MapParser;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * Main entry-point for the application.
  */
 public class Main {
 
+    static Logger Log = Logger.getLogger(Main.class.getName());
+    
     /**
      * Starts the application by initializing data and then starting server.
      * @param args  The arguments to feed the application.
      */
     public static void main(String[] args) {
+        // Log start
+        Log.finest("Main starting up.");
+        
         // Find the files
         File nodeFile = new File("krak/kdv_node_unload.txt");
         File edgeFile = new File("krak/kdv_unload.txt");
 
+        Log.finest("Starting import of map-data.");
+        
         // Import data
         MapModel model = MapParser.load(nodeFile, edgeFile);
-
-        System.out.println("Done!");
+        
+        Log.finest("Import of map-data done.");
 
         // Init server
         boolean success = WebServer.init();
+
+        // Log statis
+        if (success) {
+            Log.info("Program initialized correct and web-server has been started.");
+        } else {
+            Log.warning("Program initialized correct but web-server failed to start.");
+        }
     }
 
 }
