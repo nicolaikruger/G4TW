04184d9d31133a9fa8540f6c5ba71152567a0f6e
Jegp <jensep@gmail.com>
2012-04-09 11:08:03 +0200

    Added Vector and Transform Added a Controller Added pan and zoom-methods

---
+++ www/Controller.js
@@ -0,0 +1,42 @@
+var Controller = (function() {
+    var isLeftMouseDown, startPoint;
+    var canvas = document.getElementById('canvas');
+
+    var getCoordinates = function(e) {
+        var x = 0;
+        var y = 0;
+        if (!e) e = window.event;
+        if (e.pageX || e.pageY) 	{
+        x = e.pageX;
+        y = e.pageY;
+        }
+        else if (e.clientX || e.clientY) 	{
+        x = e.clientX + document.body.scrollLeft
+          + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop
+          + document.documentElement.scrollTop;
+        }
+        // posx and posy contain the mouse position relative to the document
+        // Do something with this information
+        return Vector(x, y - 150).round(3);
+    };
+
+    canvas.onmousedown = function(e) { isLeftMouseDown = true; startPoint = getCoordinates(e); };
+    canvas.onmouseup   =  function(e) { isLeftMouseDown = false; };
+    canvas.onmousemove = function(e) {
+        if (isLeftMouseDown) {
+            var coord = getCoordinates(e);
+            var v = startPoint.subtract(coord);
+            startPoint = startPoint.subtract(v);
+            view.pan(v.reverse());
+        }
+    };
+    // Zoom
+    canvas.onmousewheel = function(e, d) {
+        var coord = getCoordinates(e);
+        view.zoom(e.wheelDelta/120, coord);
+    };
+
+    // Return empty object
+    return {};
+}());
---
+++ www/Vector.js
@@ -0,0 +1,26 @@
+var Vector = function(x, y) {
+
+  return {
+    x: x || 0,
+    y: y || 0,
+
+    add: function(v)      { return Vector(x + v.x, y + v.y); },
+    divide: function(d)   { return Vector(x / d, y / d); },
+    length: function()    { return Math.sqrt(x * x + y * y); },
+    multiply: function(s) { return Vector(x * s, y * s); },
+    reverse: function()   { return Vector(-x, -y); },
+    round: function(dec)  {
+      if (typeof dec == 'number') {
+        var log = Math.pow(10, dec);
+        return Vector(Math.round(x * log) / log, Math.round(y * log) / log);
+      } else {
+        return Vector(Math.round(x), Math.round(y));
+      }
+    },
+    subtract: function(v) { return Vector(x - v.x, y - v.y); },
+    toString: function() {
+      return "(" + x + "," + y + ")";
+    }
+  }
+
+}
