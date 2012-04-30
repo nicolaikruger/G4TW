5f826b0ef0eafc782178a2e9458ff6c0b51a1ea5
Nicolai Kruger <nicolaikr@hotmail.com>
2012-04-30 11:40:36 +0200

    Itterator understøtter nu "remove"

--- src/main/dk/itu/kf04/g4tw/model/DijkstraEdge.java
+++ src/main/dk/itu/kf04/g4tw/model/DijkstraEdge.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package dk.itu.kf04.g4tw.model;
 
 import dk.itu.kf04.g4tw.util.DynamicArray;
@@ -40,3 +41,47 @@ public abstract class DijkstraEdge implements Iterable<DijkstraEdge>, Comparable
         };
     }
 }
+=======
+package dk.itu.kf04.g4tw.model;
+
+import dk.itu.kf04.g4tw.util.DynamicArray;
+
+import java.util.Iterator;
+
+/**
+ *
+ */
+public abstract class DijkstraEdge implements Iterable<DijkstraEdge>, Comparable<DijkstraEdge> {
+
+    public abstract double getLength();
+
+    public abstract int getId();
+
+    private DynamicArray<DijkstraEdge> edges = new DynamicArray<DijkstraEdge>();
+
+    public void addEdge(DijkstraEdge e){
+           edges.add(e);
+    }
+
+    public int compareTo(DijkstraEdge o) {
+        return Double.compare(this.getLength(), o.getLength());
+    }
+
+    public Iterator<DijkstraEdge> iterator() {
+        return new Iterator<DijkstraEdge>() {
+            private int n = 0;
+            public boolean hasNext() {
+                return n < edges.length();
+            }
+
+            public DijkstraEdge next() {
+                return edges.get(n++);
+            }
+
+            public void remove() {
+                edges.remove(--n);
+            }
+        };
+    }
+}
+>>>>>>> Stashed changes
